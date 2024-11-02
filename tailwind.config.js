5607cf7e0603bd494d02b4a83d7d73e6bc8f9f3e
ID 272 <Mohamed-ID@Github.com>
2024-11-02 21:24:23 +0100

    Login Page Using Tailwindcss

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  content: [
+    "./public/**/*.{html,js}",
+    "./src/**/*.{html,js}",
+  ],
+  theme: {
+    extend: {},
+    fontFamily: {
+      "sans": ["Ubuntu", "sans-serif"]
+    },
+  },
+  plugins: [],
+};
+
+
